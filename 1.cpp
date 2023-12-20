b17c24e60d0f4205ead265eb315b10cb8b5fbf1b
Fatemeh <fatemeh.khosh1382@gmail.com>
2023-12-20 23:07:42 +0330

    file 1 fixed

--- 1.cpp
+++ 1.cpp
@@ -2,12 +2,11 @@
 using namespace std;
 
 class container {
-
 	int size;
 public:
 	float* p;
-	container(int s) :size(s){}
-	const int& getsize() { return size;}
+	container(int s) :size(s) {}
+	const int& getsize() { return size; }
 
 };
 
@@ -15,12 +14,12 @@ class vector :public container {
 
 	int call_num;
 public:
-	explicit vector(int l) :len(l),size(1 * 100){
+	explicit vector(int l) :len(l), container(1 * 100), call_num(0) {
 		p = new float();
 	}
 	int len;
-	int& getlen() const {
-		call_num ++;
+	int& getlen() {
+		call_num++;
 		return len;
 	}
 	~vector() = default;
@@ -29,12 +28,12 @@ public:
 int main() {
 
 	container c1(100);
-	vector v1 = c1;
+	vector v1(100);
 	container& r1 = v1;
 	container c2 = 100;
-	c2.getsize() = 20;
 	cout << c2.getsize();
-	vector v2 = 100;
-	v2.getlen = 40;
+	vector v2(100);
+	v2.getlen() = 40;
 	cout << v2.getlen();
+	return 0;
 }
