432746f293c35fdfd2e8fb8dc907865544dd8987
unknown <mojtababaranpur@gmail.com>
2023-12-21 13:11:52 +0330

    fix file 7

--- 7.cpp
+++ 7.cpp
@@ -1,10 +1,17 @@
+//reviewed 
 #include<stdio.h>
 int main()
 {
-	int arr[] = { 1, 2, 3, 4, 5 };
+	int arr[5] = { 1, 2, 3, 4, 5 };
 	int *p = arr;
-	++*p;
+	*p++; //instead of ++*p;
 	p += 2;
 	printf("%d", *p);
 	return 0;
 }
+
+//the line ++*p has do nothing in fact
+//so if we wanna go next of p , can use *p++
+//when ++*p exist and not exist , the result is 3 ;
+//so we can remove or replace this line 
+//if we replace ++*p with *p++ , so we can go one step forth and get 4
