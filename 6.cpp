b71b5792adaee0cb8d4959bc17f690163dfea48f
unknown <mojtababaranpur@gmail.com>
2023-12-21 13:10:28 +0330

    fix file 6

--- 6.cpp
+++ 6.cpp
@@ -1,11 +1,17 @@
-﻿#include<stdio.h>
+﻿//reviewed
+#include<stdio.h>
 int main()
 {
 	int a;
-	char *x;
-	x = (char *)&a;
+	char * x;
+	x = (char *) &a;
 	a = 512;
 	x[0] = 1;
 	printf("%d\n", a);
 	return 0;
 }
+
+// in this program, x[0] = 1 adds a with 1 and 512 + 1 = 513 was showed in output. 
+// if we use x[0] = 10 , the result will be 512 + 10 = 522, 
+
+
